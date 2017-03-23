5a5e35f2b206ffcc4eba6d161f4f7fe5fa19d69d
Janani <jananise@gmail.com>
2017-03-23 17:06:27 +0530

    modified rest controller

--- src/main/java/com/revature/controller/HolidayController.java
+++ src/main/java/com/revature/controller/HolidayController.java
@@ -3,8 +3,15 @@ package com.revature.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,9 +26,19 @@ public class HolidayController {
 	@Autowired
 	private HolidayService holidayService;
 
-	@GetMapping("/")
+	@GetMapping
 	public List<Holiday> list() {
 		return holidayService.list();
+	}
+
+	@GetMapping("/{id}")
+	public Holiday findById(@PathVariable("id") Long id) {
+		return holidayService.findById(id);
+	}
+
+	@PostMapping
+	public void save(@RequestBody Holiday holiday) {
+		holidayService.save(holiday);
 	}
 
 }
