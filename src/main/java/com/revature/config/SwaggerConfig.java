3bd64ead01614fd51078e802ab0bfc392d1f7e30
Janani <jananise@gmail.com>
2017-03-24 16:05:27 +0530

    modified paths

--- src/main/java/com/revature/config/SwaggerConfig.java
+++ src/main/java/com/revature/config/SwaggerConfig.java
@@ -1,44 +1,43 @@
 package com.revature.config;
 
-import static springfox.documentation.builders.PathSelectors.regex;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.google.common.base.Predicate;
-
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import static com.google.common.base.Predicates.or;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
 	@Bean
-	public Docket postsApi() {
+	public Docket api() {
 		// @formatter:off
 		return new Docket(DocumentationType.SWAGGER_2)
-				.groupName("public-api")
+				.groupName("lms-api")
 				.apiInfo(apiInfo())
 				.select()
-				.paths(fetchPaths()).build();
+				.paths(PathSelectors.any()).build();
 	}
 
 	// @formatter:on
-	private Predicate<String> fetchPaths() {
-		return or(regex("/holidays.*"), regex("/roles.*"), regex("/employees.*"));
-	}
+	// private Predicate<String> fetchPaths() {
+	// return or(regex("/holidays.*"), regex("/roles.*"),
+	// regex("/employees.*"));
+	// }
 
 	private ApiInfo apiInfo() {
 		// @formatter:off
 		return new ApiInfoBuilder()
 				.title("LMS API")
-				.description("LMS API reference for developers")
+				.description("Leave Management System")
 				.license("LMS License")
+				.contact("janani@revature.com")
 				.licenseUrl("http://revature.com")
 				.version("1.0").build();
 	}
