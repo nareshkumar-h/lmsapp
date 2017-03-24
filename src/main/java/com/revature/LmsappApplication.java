554e0b21c1321d82dd46edea2f941e3ac7cb5dc9
Janani <jananise@gmail.com>
2017-03-24 10:50:29 +0530

    added swagger

--- src/main/java/com/revature/LmsappApplication.java
+++ src/main/java/com/revature/LmsappApplication.java
@@ -1,19 +1,44 @@
 package com.revature;
 
+import java.time.LocalDate;
+
 import javax.sql.DataSource;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class LmsappApplication {
 	@Bean
 	public JdbcTemplate jdbcTemplate(DataSource dataSource) {
 		return new JdbcTemplate(dataSource);
 	}
 
+	@Bean
+	public Docket mainConfig() {
+		// @formatter:off
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build()
+				.pathMapping("/lms")
+				.directModelSubstitute(LocalDate.class, String.class)
+				.genericModelSubstitutes(ResponseEntity.class);
+
+	}
+
+	// @formatter:on
 	public static void main(String[] args) {
 		SpringApplication.run(LmsappApplication.class, args);
 	}
