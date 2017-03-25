5146b7be77429eceec2d15ad1102919d4dee1046
Naresh Kumar H <nareshkumarh@live.com>
2017-03-25 15:18:54 +0530

    Added @Entity Annotation

--- src/main/java/com/revature/model/Role.java
+++ src/main/java/com/revature/model/Role.java
@@ -3,6 +3,7 @@ package com.revature.model;
 import java.time.LocalDate;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -10,6 +11,7 @@ import javax.persistence.Table;
 import lombok.Data;
 
 @Data
+@Entity
 @Table(name = "ROLE")
 public class Role {
 	
